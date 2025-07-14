b0b5c052076e1a829846100e751746e1575cde5a
HasanSCULPT <hyasuf8@gmail.com>
2025-07-14 12:50:37 +0300

    Update Lung Cancer_app.py

--- Lung Cancer_app.py
+++ Lung Cancer_app.py
@@ -31,34 +31,30 @@ st.set_page_config(page_title="Lung Cancer Diagnostics App", layout="centered")
 #import base64
 #import streamlit as st
 #Define background function
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
         data = f.read()
     return base64.b64encode(data).decode()
-# Set main background from local PNG
-main_bg = get_img_as_base64("feathered_bg.png")
-# Inject CSS for main background only
-page_bg_img = f"""
-<style>
-[data-testid="stAppViewContainer"] > .main {{
-    background-image: url("data:image/png;base64,{main_bg}");    
-        
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
     background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-}}    
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
 
-[data-testid="stHeader"] {{
-    background: rgba(0,0,0,0);
-}}
+set_png_as_page_bg('background.png')
 
-[data-testid="stToolbar"] {{
-    right: 2rem;    
-}}
-</style>
-"""
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 # 🌍 Language translations
