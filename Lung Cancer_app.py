0aff544714f28577ed9eda74b6b4e62cb5aa3def
HasanSCULPT <hyasuf8@gmail.com>
2025-07-17 04:09:25 +0300

    Update Lung Cancer_app.py

--- Lung Cancer_app.py
+++ Lung Cancer_app.py
@@ -192,15 +192,7 @@ if page == "Prediction":
         ax.hist(proba, bins=10, edgecolor='k')
         ax.axvline(threshold, color='red', linestyle='--')
         st.pyplot(fig)
-         # ✅ Precomputed Permutation Importance
-        importance_data = {
-        "Feature": ["SYMPTOM_SCORE", "LIFESTYLE_SCORE", "SHORTNESS OF BREATH", "SWALLOWING DIFFICULTY", "YELLOW_FINGERS"],
-        "Importance": [0.0629, 0.0371, 0.0274, 0.0258, 0.0081]
-        }
-    importance_df = pd.DataFrame(importance_data)
-    fig3, ax3 = plt.subplots(figsize=(6, 4))
-    ax3.barh(importance_df["Feature"], importance_df["Importance"], color='teal')
-    st.pyplot(fig3)
+        
 
 # =====================================
 # Individual Prediction
