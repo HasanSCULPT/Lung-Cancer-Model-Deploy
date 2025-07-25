3ba6468ec206c3779d70eaa71af4a5b85601281a
HasanSCULPT <hyasuf8@gmail.com>
2025-07-25 17:54:53 +0300

    Update Lung Cancer_app.py

--- Lung Cancer_app.py
+++ Lung Cancer_app.py
@@ -373,6 +373,9 @@ elif page == "Prediction":
     wheezing = st.selectbox("wheezing", [0, 1])
     cough = st.selectbox("Coughing", [0, 1])
     short_breath = st.selectbox("Shortness of Breath", [0, 1])
+    # ✅ Auto Calculate Scores
+    SYMPTOM_SCORE = sum([cough, short_breath, wheezing, swallowing, chest_pain])
+    LIFESTYLE_SCORE = sum([smoking, alcohol, peer_pressure])
     symptom_score = st.slider("SYMPTOM SCORE", 0, 10, SYMPTOM_SCORE, disabled=True) # ✅ Auto-Updating Sliders
     lifestyle_score = st.slider("LIFESTYLE SCORE", 0, 5, LIFESTYLE_SCORE, disabled=True)
 
